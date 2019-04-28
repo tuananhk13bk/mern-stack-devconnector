b1a4b71e256673bab7e61802b86eb28b0f47d571
tuananhk13bk <tuananhk13bk@gmail.com>
2019-04-28 17:55:07 +0700

    Secure my keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://admin:admin123@ds121636.mlab.com:21636/dev-connector",
-  // mongoURI: "mongodb://localhost:27017/dev-connector",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
