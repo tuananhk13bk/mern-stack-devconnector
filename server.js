9948e0315090d1b834ff47762b43660a9aeb58ee
tuananhk13bk <tuananhk13bk@gmail.com>
2019-04-28 18:05:30 +0700

    Config somethings

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -10,15 +11,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(cors());
-// Use routes
-app.use("/api/user", require("./routes/api/user/user"));
-app.use("/api/profile", require("./routes/api/profile/profile"));
-app.use("/api/post", require("./routes/api/post/post"));
 // passport middleware
 app.use(passport.initialize());
 // passport config
 require("./config/passport")(passport);
-
 // DB config
 const db = require("./config/keys").mongoURI;
 
@@ -27,6 +23,20 @@ mongoose
   .connect(db, { useNewUrlParser: true, useFindAndModify: true })
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
+
+// Use routes
+app.use("/api/user", require("./routes/api/user/user"));
+app.use("/api/profile", require("./routes/api/profile/profile"));
+app.use("/api/post", require("./routes/api/post/post"));
+
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendfile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
